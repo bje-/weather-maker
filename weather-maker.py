dc203331dd09e19e2c7c477652239428186fceb8
Ben Elliston <bje@air.net.au>
2017-12-05 10:49:22 +1100

    * weather-maker.py: Add FIXME reminder about half-hour timezones.

--- weather-maker.py
+++ weather-maker.py
@@ -203,6 +203,7 @@ parser.add_argument("--hm-data", type=str, help='BoM station data file',
                     required=True)
 parser.add_argument("--hm-details", type=str, help='BoM station details file',
                     required=True)
+# FIXME: half-hour timezones (eg +9.5) do not work.
 parser.add_argument("--tz", type=float, default=10.0,
                     help='Time zone [default +10]')
 parser.add_argument("-o", "--out", type=str, help='output filename',
